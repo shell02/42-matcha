80d2b6396449c03de15e7e20c4272404ef0364f7
Tiphaine <tiphainedurand44@gmail.com>
2023-10-03 09:38:20 +0200

    🔧 Setup ESLint and Prettier

--- Front/.eslintrc.cjs
+++ Front/.eslintrc.cjs
@@ -1,18 +1,25 @@
+const prettier = require('./.prettierrc.json')
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:security/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "@typescript-eslint", 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': ['error', prettier],
   },
 }
