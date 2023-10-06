2039640734d77171c77f5c80c119b26d5b48c81b
Tiphaine <tiphainedurand44@gmail.com>
2023-10-06 15:32:57 +0200

    ✨ Add router and pages

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Search from './pages/Search'
+import Profile from './pages/Profile'
 import './App.css'
 
 function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/user' element={<Profile />} />
+        <Route path='/user/:userId' element={<Profile />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
